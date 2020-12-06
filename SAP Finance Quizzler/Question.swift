c483fcc51bef4082fff017e2db40967a480e359e
Viktor Varsano <Viktor_varsano@hotmail.com>
2020-12-06 16:04:57 +0200

    add questions and button colours and a timer

---
+++ SAP Finance Quizzler/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  SAP Finance Quizzler
+//
+//  Created by Viktor Varsano on 6.12.20.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a 
+        
+    }
+    
+}
